7db9cc7ee8cac4919eabbdd28b0f9ad7ff31f74c
Rob Antonisse <robantonisse@gmail.com>
2024-02-15 15:42:57 +0100

    Add project files.

---
+++ WisselAandrijving_2024.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		WisselAandrijving_2024.ino
+ Created:	2/15/2024 3:42:05 PM
+ Author:	gebruiker
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
